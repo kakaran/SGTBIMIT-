b168a95dca9a8fc502b6f60d41cafab8dd288a9f
Karan Kapoor <kk8658141@gmail.com>
2023-04-25 20:12:32 +0530

    update the event page

--- src/Events/Events.jsx
+++ src/Events/Events.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import './events.css'
 import { Header, Navbar, Footer, Loader } from '../Components'
 import nssImg2 from '../images/nss-img-2.png'
@@ -7,54 +7,73 @@ import { BsFillFilterCircleFill } from 'react-icons/bs'
 import useFetch from '../useFetch'
 import { useParams } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
+import axios from 'axios'
 export default function Events() {
 
 
     const { id } = useParams()
+    const [events, setEvents] = useState();
 
     const API_URL = process.env.REACT_APP_API_URL
 
-    const { data: events, isPending } = useFetch(`${API_URL}/Eventhandler/EventHandler_Single_Display/${id}`)
+    // const { data: events, isPending } = useFetch(`${API_URL}/Eventhandler/EventHandler_Single_Display/${id}`)
 
-    const event = events ? events[0] : " "
+    useEffect(() => {
+      const EventDataGet = async () =>{
+        try {
+            const data = (await axios.get(`${API_URL}/Eventhandler/EventHandler_Single_Display/${id}`)).data
+            // console.log(data);
+            setEvents(data);
+        } catch (error) {
+            console.log(error);
+        }
+      }
+      EventDataGet()
+    }, [])
+    
+
+    // console.log(event?.Years[0]?.year);
+
+    // const [eventFilter,setEventFilter] = useState(events?.Years[0]?.year);
+    // setEventFilter(events?.Years[0]?.year)
+    // console.log(eventFilter);
 
 
-
-    let i = 0
-    const count = 4
-    let eventChunks = []
-    events && (
-        event.Years.forEach((e) => {
-            e.Events.forEach((ee) => {
-                ee.Event_id.Images.forEach(() => {
-                    let res = ee.Event_id.Images.slice(i, i + count)
-                    if (res.length === 0) return
-                    eventChunks.push(res)
-                    i = i + count
-                })
-            })
-        })
-    )
-    console.log(eventChunks ? eventChunks : '')
+    // let i = 0
+    // const count = 4
+    // let eventChunks = []
+    // events && (
+    //     event.Years.forEach((e) => {
+    //         e.Events.forEach((ee) => {
+    //             ee.Event_id.images.forEach(() => {
+    //                 let res = ee.Event_id.images.slice(i, i + count)
+    //                 if (res.length === 0) return
+    //                 eventChunks.push(res)
+    //                 i = i + count
+    //             })
+    //         })
+    //     })
+    // )
+    // console.log(eventChunks ? eventChunks : '')
     return (
         <>
-            <Helmet title={event.name} />
+            <Helmet title={events.name} />
             <Header />
             <Navbar />
-            {isPending && <Loader />}
+            {/* {isPending && <Loader />} */}
             {events && <section className='event-section'>
                 <div className="event-header">
                     <div className='gradient' />
-                    <img src={`${API_URL}/Eventhandler/EventHandleR_Heder_Image/${event._id}`} alt="" />
+                    <img src={`${API_URL}/Eventhandler/EventHandleR_Heder_Image/${events._id}`} alt="" />
                     <div className="event-header-title">
-                        <p>Home {">"} Societies {">"} {event.name}</p>
-                        <h1> {event.name} </h1>
+                        <p>Home {">"} Societies {">"} {events.name}</p>
+                        <h1> {events.name} </h1>
                     </div>
                 </div>
                 <div className="about-event-section">
                     <div className="about-event">
-                        <h1>About {event.name}</h1>
-                        <p>{event.detail}</p>
+                        <h1>About {events.name}</h1>
+                        <p>{events.detail}</p>
                     </div>
                     <div className="about-event-carousel">
                         <Carousel
@@ -62,8 +81,8 @@ export default function Events() {
                             showIndicators={false}
                             width="600px"
                         >
-                            {event.images.map((image) => (
-                                <img src={`${API_URL}/Eventhandler/EventHandler_Image_Display/${event._id}/${image._id}`} alt="cant load" className='event-carousel-img' />
+                            {events.images.map((image) => (
+                                <img src={`${API_URL}/Eventhandler/EventHandler_Image_Display/${events._id}/${image._id}`} alt="cant load" className='event-carousel-img' />
                             ))}
                         </Carousel>
                     </div>
@@ -73,7 +92,7 @@ export default function Events() {
                         <button type='button'>Events</button>
                         <BsFillFilterCircleFill size="min(4rem, 5vw)" color='#005E93' />
                     </div>
-                    {event.Years.map((year, i) => (
+                    {/* {event.Years.map((year, i) => (
                         <div className="events-container">
                             <h1
                                 style={{
@@ -102,7 +121,7 @@ export default function Events() {
                                 ))}
                             </Carousel>
                         </div>
-                    ))}
+                    ))} */}
                 </section>
             </section>}
             <Footer />

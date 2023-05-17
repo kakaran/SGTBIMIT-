ccef076e9ad6d4063fcd06f0b7b413a2953c6d7b
Aditib02 <bhatiaaditi730@gmail.com>
2023-05-18 01:35:30 +0530

    registration

--- src/Alumni/Registration/Registration.jsx
+++ src/Alumni/Registration/Registration.jsx
@@ -9,69 +9,125 @@ export default function Registration() {
             <Helmet title='SGTBIMIT | Alumini Registeration' />
             <Header />
             <Navbar />
-            <section className="registeration-section">
-                <h1>ALUMINI REGISTRATION FORM</h1>
-                <form className="personal-details">
+            <div className="container">
+                <div className="title">Registration</div>
+                <div className="content">
 
-                    <h2>PERSONAL DETAILS</h2>
-                    <label for="fname">First Name</label>
-                    <input type="text" name="fname" id="fname" />
-                    <label for="lname">Last Name</label>
-                    <input type="text" name="lname" id="lname" />
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" />
-                    <label for="Mobile">Mobile No.</label>
-                    <input type="text" name="mobile" id="Mobile" />
-                    <label for="Address">Address</label>
-                    <input type="text" name="address" id="Address" />
-                    <label for="Adhaar">Adhaar No.</label>
-                    <input type="text" name="aadhaar" id="Adhaar" />
-
-
-                    <h2>HIGHER EDUCATION DETAILS</h2>
-
-                    <label for="higher-studies">Higher Studies</label>
-                    <select name="select">
-                        <option value="BCA">BCA</option>
-                        <option value="BBA">BBA</option>
-                        <option value="BBA (B&I)">BBA(B&I)</option>
-                        <option value="BCOMM">BCOMM</option>
-                    </select>
-                    <label for="Year">Year</label>
-                    <input type="text" id="Year" name="year" />
-                    <label for="Employed">Employed</label>
-                    <div>
-                        <div>
-                            <input type="radio" id='yes' name='employed' value="Yes" />
-                            <label for="yes">YES</label>
+                    <form>
+                        <div className="title2">PERSONAL DETAILS</div>
+                        <div className="user-details">
+                            <div className="input-box">
+                                <span className="details">First Name</span>
+                                <input type="text" placeholder="Enter your name" required></input>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Last Name</span>
+                                <input type="text" placeholder="Enter your Last name" required></input>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Email</span>
+                                <input type="text" placeholder="Enter your email" required></input>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Mobile Number</span>
+                                <input type="text" placeholder="Enter your number" required></input>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Address</span>
+                                <input type="text" placeholder="Enter your Address" required></input>
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Adhaar Number</span>
+                                <input type="text" placeholder="Enter your Adhaar number" required></input>
+                            </div>
                         </div>
-                        <div>
-                            <input type="radio" id="No" name='employed' value="No" />
-                            <label for="No">NO</label>
-                        </div>
-                    </div>
+                        <div className="gender-details">
+                            <input type="radio" name="gender" id="dot-1"></input>
+                            <input type="radio" name="gender" id="dot-2"></input>
+                            <input type="radio" name="gender" id="dot-3"></input>
+                            <span className="gender-title">Gender</span>
+                            <div className="category">
+                                <label for="dot-1">
+                                    <span className="dot one"></span>
+                                    <span className="gender">Male</span>
+                                </label>
+                                <label for="dot-2">
+                                    <span className="dot two"></span>
+                                    <span className="gender">Female</span>
+                                </label>
+                                <label for="dot-3">
+                                    <span className="dot three"></span>
+                                    <span className="gender">Prefer not to say</span>
+                                </label>
+                            </div>
+                        </div><br />
+                        <div className="title2">HIGHER EDUCATION DETAILS</div>
+                        <div className="edu-details">
+                            <input type="radio" name="high" id="dot1" />
+                            <input type="radio" name="high" id="dot2" />
+                            <input type="radio" name="high" id="dot3" />
+                            <input type="radio" name="high" id="dot4" />
+                            <span className="details3">Higher Education</span>
+                            <div className="cat">
+                                <label for="dot1">
+                                    <span className="dot one"></span>
+                                    <span className="high">BCA</span>
+                                </label>
+                                <label for="dot2">
+                                    <span className="dot two"></span>
+                                    <span className="high">BBA</span>
+                                </label>
+                                <label for="dot3">
+                                    <span className="dot three"></span>
+                                    <span className="high">BBA(B&I)</span>
+                                </label>
+                                <label for="dot4">
+                                    <span className="dot four"></span>
+                                    <span className="high">BCOMM</span>
+                                </label>
+                            </div>
+                        </div><br />
 
-                    <h2>CURRENT WORKING DETAILS</h2>
-
-                    <label for="Placement">Placement Provided by SGTBIMIT</label>
-                    <div>
-                        <div>
-                            <input type="radio" id='yes-p' name='placement' value="Yes" />
-                            <label for="yes-p">YES</label>
+                        <label for="Year" className="details3">Year</label>
+                        <div className="input3">
+                            <input type="text" placeholder="Enter Year" required></input>
                         </div>
+                        <label for="Employed">Employed</label>
                         <div>
-                            <input type="radio" id="No-p" name='placement' value="No" />
-                            <label for="No-p">NO</label>
+                            <div>
+                                <input type="radio" id="yes" name="employed" value="Yes" />
+                                <label for="yes">YES</label>
+                                </div>
+                                <div>
+                                <input type="radio" id="no" name="employed" value="no" />
+                                <label for="no">NO</label>
+                                </div>
+                        </div>  
+                        <div className='title2'>CURRENT WORKING DETAILS </div>
+                        <div className='working-data' >
+                            <label for="placement">Placement provided by SGTBIMIT</label>
+                            <div>
+                                <div>
+                                    <input type="radio" id="yes-p" name="Placement" value="yes" />
+                                    <label for="yes-p">YES</label>
+                                </div>
+                                <div>
+                                    <input type="radio" id="no-p" name="Placement" value="no" />
+                                    <label for="no-p">NO</label>
+                                </div>
+                            </div>
+                            <label for="Organization">Present Organization</label>
+                            <input type="text" /> <br/>
+                            <label for= "Designation">Current Designation</label>
+                            <input type="text" />     
+                        </div>                          
+                                <div className="button">
+                                    <input type="submit" value="Register"></input>
+                                </div>
+                            </form>
                         </div>
-                    </div>
-                    <label for="Organization"> Present Organization</label>
-                    <input type="text" />
-                    <label for="Designation">Current Designation</label>
-                    <input type="text" />
-                    <button>Submit</button>
-                </form>
-            </section>
-            <Footer />
-        </>
-    )
+                </div>
+                <Footer />
+            </>
+            )
 }

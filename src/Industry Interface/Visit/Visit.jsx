df059d83f108536b554d3c41ca2db4bec94595ab
kakaran <kk8658141@gmail.com>
2023-06-08 23:48:38 +0530

    industral visit data upload

--- src/Industry Interface/Visit/Visit.jsx
+++ src/Industry Interface/Visit/Visit.jsx
@@ -1,77 +1,112 @@
-import React, { useEffect, useState } from 'react'
-import { Footer, Header, Loader, Navbar } from '../../Components'
-import { motion } from 'framer-motion'
-import { routingAnimations } from '../../constants'
-import styles from './visit.module.css'
+import React, { useEffect, useState } from "react";
+import { Footer, Header, Loader, Navbar } from "../../Components";
+import { motion } from "framer-motion";
+import { routingAnimations } from "../../constants";
+import styles from "./visit.module.css";
+import axios from "axios";
 
 const Visit = () => {
-    const [visit, setVisit] = useState(null)
+  const [visit, setVisit] = useState(null);
 
-    const getVisitData = async () => {
-        try {
-            const res = await fetch(`${import.meta.env.VITE_API_URL}/IndustrialVisits/IndustrialVisits_Display`)
-            const data = await res.json()
-            setVisit(data)
-            // console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
+  const getVisitData = async () => {
+    try {
+      const Data = (await axios.get(`${import.meta.env.VITE_API_URL}/IndustrialVisits/IndustrialVisits_Display`)).data;
+      console.log(Data);
+      setVisit(Data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        getVisitData()
-    }, [])
+  useEffect(() => {
+    getVisitData();
+  }, []);
 
-    return (
-        <>
-            <Header />
-            <Navbar />
-            <motion.section
-                viewport="viewport"
-                initial="initial"
-                animate="animate"
-                exit="exit"
-                transition="transition"
-                variants={routingAnimations}
-                className='w-[min(1500px,98%)] mx-auto'
-            >
-                <div className='gradient-bg'></div>
-                <div className='w-[min(1500px,98%)] mx-auto'>
-                    <h1 className='my-bold my-text-4 text-center primary-blue'>Industrial Visit</h1>
-                    <p className='text-2xl text-gray-800'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis repellendus beatae sit tenetur aliquam animi nesciunt fuga veritatis, quis placeat ad aperiam nihil ut libero pariatur expedita? Veritatis magnam delectus qui excepturi a odio, accusamus accusantium, tempore officia eius in illo hic, quidem inventore incidunt! Illo ad quaerat nesciunt perspiciatis. Accusamus omnis atque reprehenderit! Corrupti, aspernatur magnam? Reprehenderit, ipsum dignissimos, esse assumenda aut nulla at commodi quisquam dolorum soluta alias repellendus, autem explicabo sint libero perferendis repellat quibusdam ab minus eveniet. Ratione error molestiae officiis magnam atque deleniti tenetur similique dolorum. Facere vel enim molestiae a necessitatibus officiis, beatae ducimus?</p>
-                </div>
-                {/* <div>
-                    <h1 className='my-bold text-5xl max-md:text-3xl primary-blue m-0'>Companies Visited</h1>
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <motion.section
+        viewport="viewport"
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition="transition"
+        variants={routingAnimations}
+        className="w-[min(1500px,98%)] mx-auto"
+      >
+        <div className="gradient-bg"></div>
+        <div className="w-[min(1500px,98%)] mx-auto">
+          <h1 className="my-bold my-text-4 primary-blue text-center uppercase">
+            Industrial Visit
+          </h1>
+          <p className=" text-2xl text-justify text-gray-800">
+            Industrial visit is a part of the college curriculum and holds a
+            special place in the professional career of students. It is a
+            standard process in engineering and MBA courses where students get
+            insight into real-world problems. Students understand the internal
+            working of an organization through an industrial visit. During the
+            industrial visits, students get to learn how professionals perform
+            at work and they implement certain industry-specific concepts such
+            as benchmarking, quality analysis, just-in-time production, etc An
+            industrial visit gives students a brief knowledge about what they
+            are interested in, the students will get to know the working
+            environment in the industries, their processing, how machines are
+            working, interact with the workers, The industrial visit is
+            considered one of the most tactical methods of teaching. It provides
+            students with an opportunity to learn practically through
+            interaction, working methods and employment practices. It gives
+            exposure from an academic point of view.
+          </p>
+        </div>
+        {/* <div>
+                    <h1 className='my-bold max-md:text-3xl primary-blue m-0 text-5xl'>Companies Visited</h1>
                     <div className={`${styles.line} w-full h-1 primary-blue-bg-clr`}></div>
                     <div className={`${styles.container} px-10 py-20 shadow-2xl mx-auto`}>
-                        <p className='text-justify text-2xl primary-blue'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut distinctio tempora natus eveniet rerum! Voluptatibus, odio magni. Ea, reiciendis reprehenderit voluptas quos delectus provident asperiores nostrum aperiam dicta quod, laborum odio eum nobis, perferendis veniam. Ut odit iusto unde, eius, vitae cumque, reiciendis inventore non autem sed aut debitis eum!</p>
+                        <p className='primary-blue text-2xl text-justify'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut distinctio tempora natus eveniet rerum! Voluptatibus, odio magni. Ea, reiciendis reprehenderit voluptas quos delectus provident asperiores nostrum aperiam dicta quod, laborum odio eum nobis, perferendis veniam. Ut odit iusto unde, eius, vitae cumque, reiciendis inventore non autem sed aut debitis eum!</p>
                     </div>
                 </div> */}
-                <div>
-                    {!visit && <Loader />}
-                    {visit?.map(visit => (
-                        <div key={visit._id}>
-                            <h1 className='text-5xl my-bold blue_gradient m'>{visit.name}</h1>
-                            <div className={`w-full h-1 primary-bg-clr rounded-full`}></div>
-                            <div className='h-28' />
-                            <div className='flex'>
-                                <div className='w-3/5  z-10'>
-                                    <img src={`${import.meta.env.VITE_API_URL}/IndustrialVisits/IndustrialVisits_Image_Display/${visit._id}`} alt="cant load" className='w-full aspect-video rounded-lg shadow-lg ' style={{
-                                        transform: "translate(3rem,-30%)"
-                                    }} />
-                                    <img src={`${import.meta.env.VITE_API_URL}/IndustrialVisits/IndustrialVisits_CompanyImg_Display/${visit._id}`} alt="" className='w-full aspect-square' />
-                                </div>
-                                <p className='bg-[#FFDFDF] z-0 p-16 rounded-lg text-gray-700 text-justify text-2xl flex items-center'>
-                                    {visit.about}
-                                </p>
-                            </div>
-                        </div>
-                    ))}
+        <div>
+          {!visit && <Loader />}
+          {visit?.map((visit) => (
+            <div key={visit._id}>
+              <h1 className="my-bold blue_gradient m text-5xl">{visit.name}</h1>
+              <div className={`w-full h-1 primary-bg-clr rounded-full`}></div>
+              <div className="h-28" />
+              <div className="flex">
+                <div className="z-10 w-3/5">
+                  <img
+                    src={`${
+                      import.meta.env.VITE_API_URL
+                    }/IndustrialVisits/IndustrialVisits_Image_Display/${
+                      visit._id
+                    }`}
+                    alt="cant load"
+                    className="aspect-video w-full rounded-lg shadow-lg"
+                    style={{
+                      transform: "translate(3rem,-30%)",
+                    }}
+                  />
+                  <img
+                    src={`${
+                      import.meta.env.VITE_API_URL
+                    }/IndustrialVisits/IndustrialVisits_CompanyImg_Display/${
+                      visit._id
+                    }`}
+                    alt=""
+                    className="aspect-square w-full"
+                  />
                 </div>
-            </motion.section>
-            <Footer />
-        </>
-    )
-}
-
-export default Visit
+                <p className="bg-[#FFDFDF] z-0 p-16 rounded-lg text-gray-700 text-justify text-2xl flex items-center">
+                  {visit.about}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </motion.section>
+      <Footer />
+    </>
+  );
+};
+export default Visit;
